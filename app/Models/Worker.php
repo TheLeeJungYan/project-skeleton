f4e604c208d4242f81f5206020730271fa553d36
leejungyan08 <123609381+TheLeeJungYan@users.noreply.github.com>
2024-12-14 00:38:22 +0800

    added softdelete to Worker model

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Worker extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'workers';
+
+    protected $fillable = [
+        'firstName','lastName','email'
+    ];
+
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    public function employmentHistories(){
+        return $this->hasMany(WorkerEmploymentHistories::class);
+    }
 }
